d6f7065a6a0d1cb2814042b0a188d0371f22ebc0
Stuart Auchterlonie <stuarta@mythtv.org>
2019-10-24 14:09:08 +0100

    Update host model to ensure destroy works

--- app/models/legacy/host.rb
+++ app/models/legacy/host.rb
@@ -1,6 +1,6 @@
 class Legacy::Host < ActiveRecord::Base
   self.table_name = 'host'
 
-  has_many :file_system
-  has_many :host_link
+  has_many :file_system, dependent: :destroy
+  has_many :host_link, foreign_key: "host_link_id", dependent: :destroy
 end
