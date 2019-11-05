39e8b348e69bbe9d7c79a69914a7be338c99cc16
Stuart Auchterlonie <stuarta@mythtv.org>
2019-11-05 13:40:29 +0000

    Finished processing messages are informational

--- app/jobs/process_host_job.rb
+++ app/jobs/process_host_job.rb
@@ -4,7 +4,7 @@ class ProcessHostJob < ApplicationJob
   def perform(host_submission)
     hostp = Processor::HostProcessor.new
     host = hostp.process_host(host_submission.data)
-    Rails.logger.debug "Finished processing host uuid: #{host.uuid}, public: #{host.pub_uuid}"
+    Rails.logger.info "Finished processing host uuid: #{host.uuid}, public: #{host.pub_uuid}"
     host_submission.added = true
     host_submission.save
   end
