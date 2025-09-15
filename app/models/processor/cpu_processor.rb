e8dac74a6fe9265a14f3802b0afa3cd49784e1e7
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-09-15 18:31:46 +0100

    Add cpu processor

---
+++ app/models/processor/cpu_processor.rb
@@ -0,0 +1,12 @@
+class Processor::CpuProcessor
+  def find_or_create_cpu!(family, stepping, model_num, vendor, model)
+    begin
+      c = Core::Cpu.find_or_create_by!(cpu_family: family, cpu_stepping: stepping, cpu_model_num: model_num, cpu_vendor: vendor, cpu_model: model)
+    rescue ActiveRecord::RecordNotUnique => e
+      retry
+    end
+    c.save!
+    # Return cpu object
+    c
+  end
+end
