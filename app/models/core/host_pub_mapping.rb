664202cebdffd5e663044b1190d5aa5dfd4cefe5
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-09-16 00:35:03 +0100

    Remove legacy database from HostPubMapping

--- app/models/core/host_pub_mapping.rb
+++ app/models/core/host_pub_mapping.rb
@@ -9,19 +9,13 @@ class Core::HostPubMapping < ApplicationRecord
   end
 
   def get_pub_uuid(uuid)
-    host = Legacy::Host.find_by_uuid(uuid)
-    if host.nil?
+    mapping = Core::HostPubMapping.find_by_uuid(uuid)
+    if mapping.nil?
       Rails.logger.debug "Failed to find existing host"
-      # Look for an existing mapping of host -> pub_uuid
-      mapping = Core::HostPubMapping.find_by_uuid(uuid)
-      if mapping.nil?
-        pub_uuid = create_pub_uuid(uuid)
-      else
-        Rails.logger.debug "Found an existing mapping #{mapping["uuid"]} to #{mapping["pub_uuid"]}"
-        pub_uuid = mapping["pub_uuid"]
-      end
+      pub_uuid = create_pub_uuid(uuid)
     else
-      pub_uuid = host.pub_uuid
+      Rails.logger.debug "Found an existing mapping #{mapping["uuid"]} to #{mapping["pub_uuid"]}"
+      pub_uuid = mapping["pub_uuid"]
     end
     pub_uuid
   end
