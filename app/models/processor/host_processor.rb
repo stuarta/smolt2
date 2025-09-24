23761dda561aecc27adbaa0212df9575a193f78a
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-09-24 20:50:38 +0100

    Update host processing to use new associations

--- app/models/processor/host_processor.rb
+++ app/models/processor/host_processor.rb
@@ -1,64 +1,78 @@
 class Processor::HostProcessor
   def process_host(host_raw)
     ActiveRecord::Base.transaction do
-      ts = DateTime.now
       host = JSON.parse(host_raw)
       Rails.logger.debug "Processing host #{host["uuid"]}"
-      # Create Host entry
-      h = Legacy::Host.find_by_uuid(host["uuid"])
-      if h.nil?
-        mapper = Core::HostPubMapping.new
-        pub_uuid = mapper.get_pub_uuid(host["uuid"])
-        h = Legacy::Host.new(
-          uuid:             host["uuid"],
-          pub_uuid:         pub_uuid,
-          last_modified:    ts,
-          rating:           0,
-          selinux_enabled:  host["selinux_enabled"]
+      hpm = Core::HostPubMapping.find_or_create_by(uuid: host["uuid"])
+      begin
+        cpu = Core::Cpu.find_or_create_by!(
+          cpu_family:     host["cpu_family"],
+          cpu_stepping:   host["cpu_stepping"],
+          cpu_model_num:  host["cpu_model_num"],
+          cpu_vendor:     host["cpu_vendor"],
+          cpu_model:      host["cpu_model"]
         )
-        Core::HostPubMapping.where(:uuid => host["uuid"]).destroy_all
+        ff = Core::Formfactor.find_or_create_by!(formfactor: host["formfactor"])
+        k_ver = Core::KernelVersion.find_or_create_by!(kernel_version: host["kernel_version"])
+        platform = Core::Platform.find_or_create_by!(platform: host["platform"])
+        vendor = Core::Vendor.find_or_create_by!(name: host["vendor"])
+        runlevel = Core::RunLevel.find_or_create_by!(default_runlevel: host["default_runlevel"])
+        operating_system = Core::OperatingSystem.find_or_create_by!(os: host["os"])
+        language = Core::Language.find_or_create_by!(language: host["language"])
+      rescue ActiveRecord::RecordNotUnique
+        retry
       end
-      h.update(
-        os:               host["os"],
-        platform:         host["platform"],
+      # Create Host entry
+      h = Core::Host.new(
         bogomips:         host["bogomips"],
+        system:           host["system"],
         system_memory:    host["system_memory"],
         system_swap:      host["system_swap"],
-        vendor:           host["vendor"],
-        system:           host["system"],
-        cpu_vendor:       host["cpu_vendor"],
-        cpu_model:        host["cpu_model"],
         num_cpus:         host["num_cpus"],
         cpu_speed:        host["cpu_speed"],
-        language:         host["language"],
-        default_runlevel: host["default_runlevel"],
-        kernel_version:   host["kernel_version"],
-        formfactor:       host["formfactor"],
-        last_modified:    ts,
         selinux_enabled:  host["selinux_enabled"],
         selinux_policy:   host["selinux_policy"],
         selinux_enforce:  host["selinux_enforce"],
-        cpu_stepping:     host["cpu_stepping"],
-        cpu_family:       host["cpu_family"],
-        cpu_model_num:    host["cpu_model_num"],
       )
-      h.save!
+      h.host_pub_mapping_id = hpm.id
+      h.cpu_id = cpu.id
+      h.formfactor_id = ff.id
+      h.kernel_version_id = k_ver.id
+      h.platform_id = platform.id
+      h.vendor_id = vendor.id
+      h.run_level_id = runlevel.id
+      h.operating_system_id = operating_system.id
+      h.language_id = language.id
       # Create Devices and link back to host
       host["devices"].each do |device|
         devp = Processor::DeviceProcessor.new
         dev = devp.find_or_create_device!(device)
-        link = Legacy::HostLink.find_or_create_by!(host: h, device: dev)
+        h.devices << dev
       end
       # Process filesystems
       host["fss"].each do |fs|
-        f = Processor::FileSystemProcessor.new
-        f.process_filesystem!(h, fs)
+        if fs["mnt_pnt"] != "WITHHELD"
+          h.file_systems.build(
+            mnt_pnt: fs["mnt_pnt"],
+            fs_type: fs["fs_type"],
+            fs_type: fs["fs_type"],
+            f_bavail: fs["f_favail"],
+            f_bsize: fs["f_bsize"],
+            f_frsize: fs["f_frsize"],
+            f_blocks: fs["f_blocks"],
+            f_bfree: fs["f_bfree"],
+            f_files: fs["f_files"],
+            f_ffree: fs["f_ffree"],
+            f_fssize: fs["f_fssize"],
+          )
+        end
       end
       # Process MythTV specific data
-      m = Processor::MythtvProcessor.new
-      m.process_mythtv!(h, host["distro_specific"]["mythtv"])
+#      m = Processor::MythtvProcessor.new
+#      m.process_mythtv!(h, host["distro_specific"]["mythtv"])
       Rails.logger.debug "Reached the end of processing"
 
+      h.save
       # Return host object
       h
     end
