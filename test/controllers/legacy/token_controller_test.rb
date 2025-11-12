5b490eec27658e3b377003d892db6dfc19d51b6b
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-11-12 18:18:42 +0000

    fix legacy test cases

--- test/controllers/legacy/token_controller_test.rb
+++ test/controllers/legacy/token_controller_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class Legacy::TokenControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   test "token_json_works" do
     get token_json_url, params: { uuid: "cf86e924-c797-41a5-a9fe-857be25979fa" }
     assert_response :success
