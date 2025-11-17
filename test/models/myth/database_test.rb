1ea8f691894c4ea965dab86673d275c014cf63a6
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-11-17 21:25:09 +0000

    Myth::Database tests

--- test/models/myth/database_test.rb
+++ test/models/myth/database_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class Myth::DatabaseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Myth::Database associations" do
+    mj = file_fixture("mythtv.json").read
+    m = JSON.parse(mj)
+    db = m["features"]["database"]
+    x = Myth::Database.new
+
+    db["schema"].each do |sk, sv|
+      skv = Myth::Schemaversion.find_or_create_by(name: "#{sk}", version: "#{sv}")
+      x.schemaversions<<skv
+    end
+
+    db["engines"].each do |sv|
+      ev = Myth::DbEngine.find_or_create_by(engine: "#{sv}")
+      x.db_engines<<ev
+    end
+
+    ev = Myth::DbEngine.find_or_create_by(engine: db["usedengine"])
+    x.used_engine=ev
+
+    dbv = Myth::DbVersion.find_or_create_by(version: db["version"])
+    x.db_version_id = dbv.id
+
+    assert x.save
+  end
 end
