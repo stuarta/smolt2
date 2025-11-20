afad2905a2332170eaa0113ed6bd0b249e5e75aa
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-11-20 22:40:12 +0000

    Myth::Audio associations

--- db/migrate/20251001211344_create_myth_audios.rb
+++ db/migrate/20251001211344_create_myth_audios.rb
@@ -1,21 +1,24 @@
 class CreateMythAudios < ActiveRecord::Migration[8.0]
   def change
     create_table :myth_audios do |t|
-      t.string :passthru
-      t.integer :stereopcm
-      t.string :upmixtype
-      t.integer :volcontrol
+      t.string  :audio_sys
+      t.string  :audio_sys_version
       t.integer :defaultupmix
-      t.integer :passthruoverride
-      t.string :mixercontrol
-      t.integer :sr_override
-      t.string :passthrudevice
-      t.string :device
-      t.string :mixerdevice
-      t.string :audio_sys
-      t.string :audio_sys_version
+      t.string  :device
       t.integer :jack
+      t.integer :maxchannels
+      t.string  :mixercontrol
+      t.string  :mixerdevice
+      t.string  :passthru
+      t.string  :passthrudevice
+      t.integer :passthruoverride
       t.integer :pulse
+      t.integer :sr_override
+      t.integer :stereopcm
+      t.string  :upmixtype
+      t.integer :volcontrol
+
+      t.belongs_to :host
 
       t.timestamps
     end
