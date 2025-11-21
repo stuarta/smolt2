6730c70f18b0b95e4edef73a83530eb15a139cf0
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-11-21 17:47:54 +0000

    Myth::Version version bucket handling test

--- test/models/myth/version_test.rb
+++ test/models/myth/version_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class Myth::VersionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "mythtv version bucket handline" do
+    dbv = Myth::Version.find_or_create_by(version: "v31.0+fixes.202203040618.7e4ce1ba98~ubuntu20.04.1")
+    assert dbv.version_bucket == "v31.0"
+  end
 end
