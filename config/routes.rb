b2c2931137cc684f97f1519d451c24b5a4ab80df
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-12-10 19:48:14 +0000

    Update routes for reports, stats, and to fix paths for client controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,22 @@ Rails.application.routes.draw do
     get "admin_token_json", to: "legacy/token#admin_token_json"
   end
   scope "/client" do
-    post "add_json",                    to: "legacy/client#add_json"
-    post "delete/(:uuid)",              to: "legacy/client#delete"
-    get "show/(:uuid)",                 to: "legacy/client#show"
+    post "add_json",            to: "legacy/client#add_json"
+    post "delete",              to: "legacy/client#delete"
+    get "show",                 to: "legacy/client#show"
     # FIXME: show_all does normally return more data than show
-    get "show_all/:uuid",               to: "legacy/client#show"
-    get "pub_uuid/(:uuid)",             to: "legacy/client#pub_uuid"
-    get "regenerate_pub_uuid/(:uuid)",  to: "legacy/client#regenerate_pub_uuid"
+    get "show_all",             to: "legacy/client#show"
+    get "pub_uuid",             to: "legacy/client#pub_uuid"
+    get "regenerate_pub_uuid",  to: "legacy/client#regenerate_pub_uuid"
   end
+  get "reports/recent"
+  get "reports/search_profiles"
+  get "reports/view_profile"
+  get "reports/view_profiles"
+  get "reports/search_devices"
+  get "reports/view_device"
+  get "reports/view_devices"
+  get "reports/search"
+  get "stats/hosts"
+  get "stats/devices"
 end
