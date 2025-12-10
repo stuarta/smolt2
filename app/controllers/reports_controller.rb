955df682da6561f6478733374c30b50ead49c509
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-12-10 22:13:44 +0000

    reports_view_profile tests

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,6 +8,23 @@ class ReportsController < ApplicationController
   end
 
   def view_profile
+    @host = if not params[:id].nil?
+      Core::Host.find(params[:id])
+    elsif not params[:uuid].nil?
+      if params[:uuid].start_with?("pub_")
+        hpm = Core::HostPubMapping.where(pub_uuid: params[:uuid]).last
+        if not hpm.nil?
+          Core::Host.where(host_pub_mapping_id: hpm.id)
+        end
+      else
+        hpm = Core::HostPubMapping.where(uuid: params[:uuid]).last
+        if not hpm.nil?
+          Core::Host.where(host_pub_mapping_id: hpm.id)
+        end
+      end
+    else
+      nil
+    end
   end
 
   def view_profiles
--- test/controllers/reports_controller_test.rb
+++ test/controllers/reports_controller_test.rb
@@ -11,6 +11,19 @@ class ReportsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get view_profile" do
+    get reports_view_profile_url, params: { id: 1 }
+    assert_response :success
+    get reports_view_profile_url, params: { uuid: "11111111-1111-1111-1111-111111111111" }
+    assert_response :success
+    get reports_view_profile_url, params: { uuid: "pub_11111111-1111-1111-1111-111111111111" }
+    assert_response :success
+    get reports_view_profile_url, params: { uuid: "1234" }
+    assert_match "Host not found", @response.body
+    get reports_view_profile_url, params: { uuid: "pub_1234" }
+    assert_match "Host not found", @response.body
+  end
+
   test "should get view_profiles" do
     get reports_view_profiles_url
     assert_response :success
