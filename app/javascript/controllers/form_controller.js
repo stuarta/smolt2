7a2b42db4cac21e9a5f168f601d84df2e99d7296
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-12-14 23:38:00 +0000

    Add css_zero form

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,56 @@
+import { Controller } from "@hotwired/stimulus"
+import debounce from "https://esm.sh/just-debounce-it@3.2.0?standalone"
+
+export default class extends Controller {
+  static targets = [ "cancel", "submit" ]
+  static values = { debounceTimeout: { type: Number, default: 300 } }
+
+  initialize() {
+    this.debouncedSubmit = debounce(this.debouncedSubmit.bind(this), this.debounceTimeoutValue)
+  }
+
+  submit({ params: { submitter } }) {
+    if (submitter) {
+      this.element.requestSubmit(this.#find(submitter))
+    } else {
+      this.element.requestSubmit()
+    }
+  }
+
+  debouncedSubmit(event) {
+    this.submit(event)
+  }
+
+  submitToTopTarget() {
+    this.element.setAttribute("data-turbo-frame", "_top")
+    this.submit()
+  }
+
+  disableSubmitWhenInvalid() {
+    if (this.element.checkValidity()) {
+      this.submitTarget.removeAttribute("disabled")
+    } else {
+      this.submitTarget.toggleAttribute("disabled", true)
+    }
+  }
+
+  reset() {
+    this.element.reset()
+  }
+
+  cancel() {
+    this.cancelTarget?.click()
+  }
+
+  preventAttachment(event) {
+    event.preventDefault()
+  }
+
+  #find(id) {
+    return document.getElementById(id) || this.#notFound(id)
+  }
+
+  #notFound(id) {
+    throw new Error(`Submitter with ID "${id}" not found`)
+  }
+}
