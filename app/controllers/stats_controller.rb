1a94376045adcfe7a4d012fb17dd2c52dce8b7c4
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-12-15 23:32:09 +0000

    Basic Host stats page

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,14 @@
 class StatsController < ApplicationController
   def hosts
+    @host_stats = {}
+    @host_stats["total"] = Core::Host.count
+    date_now = Date.today
+    date_minus_90d = date_now - 90
+    @host_stats["active"] = Core::Host.where("updated_at > ?", date_minus_90d).count
+    @host_stats["device_total"] = Core::Device.count
+    @host_stats["bogomips"] = Core::Host.sum(:bogomips)
+    @host_stats["processors"] = Core::Host.sum(:num_cpus)
+    @host_stats["mhz"] = Core::Host.sum(:cpu_speed)
   end
 
   def devices
