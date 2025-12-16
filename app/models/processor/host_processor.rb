e3de3c93f7f83d60c9bdd58ddf6db372140f1333
Stuart Auchterlonie <stuarta@squashedfrog.net>
2025-12-16 22:06:05 +0000

    Only update arrival on a valid device

--- app/models/processor/host_processor.rb
+++ app/models/processor/host_processor.rb
@@ -61,8 +61,8 @@ class Processor::HostProcessor
       host["devices"].each do |device|
         devp = Processor::DeviceProcessor.new
         dev = devp.find_or_create_device!(device)
-        dev.updated_at = host_raw.arrival
         if not dev.nil?
+          dev.updated_at = host_raw.arrival
           h.devices << dev
         end
       end
