e761d54786c8cc93efd7df21a4f6d969f589caa6
Stuart Auchterlonie <stuarta@squashedfrog.net>
2026-01-21 17:34:55 +0000

    Add bigger memory buckets

--- app/models/concerns/memory_bucket_concern.rb
+++ app/models/concerns/memory_bucket_concern.rb
@@ -2,11 +2,13 @@ module MemoryBucketConcern
   extend ActiveSupport::Concern
 
   included do
-    scope :memory_0_to_2Gb, -> { where("system_memory < 2048") }
-    scope :memory_2_to_4Gb, -> { where("system_memory >= 2048 AND system_memory < 4096") }
-    scope :memory_4_to_8Gb, -> { where("system_memory >= 4096 AND system_memory < 8192") }
-    scope :memory_8_to_16Gb, -> { where("system_memory >= 8192 AND system_memory < 16384") }
-    scope :memory_16_to_32Gb, -> { where("system_memory >= 16384 AND system_memory < 32768") }
-    scope :memory_32Gb_plus, -> { where("system_memory >= 32768") }
+    scope :memory_0_to_2Gb,    -> { where("system_memory < 2048") }
+    scope :memory_2_to_4Gb,    -> { where("system_memory >= 2048 AND system_memory < 4096") }
+    scope :memory_4_to_8Gb,    -> { where("system_memory >= 4096 AND system_memory < 8192") }
+    scope :memory_8_to_16Gb,   -> { where("system_memory >= 8192 AND system_memory < 16384") }
+    scope :memory_16_to_32Gb,  -> { where("system_memory >= 16384 AND system_memory < 32768") }
+    scope :memory_32_64Gb,     -> { where("system_memory >= 32768 AND system_memory < 65536") }
+    scope :memory_64_to_128Gb, -> { where("system_memory >= 65536 AND system_memory < 131072") }
+    scope :memory_128Gb_plus,  -> { where("system_memory >= 131072") }
   end
 end
