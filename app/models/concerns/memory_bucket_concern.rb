971ab4d93285b45796d4084ae3b056cdd60ec1e7
Stuart Auchterlonie <stuarta@squashedfrog.net>
2026-01-21 17:41:58 +0000

    Fix mem bucket concern

--- app/models/concerns/memory_bucket_concern.rb
+++ app/models/concerns/memory_bucket_concern.rb
@@ -7,7 +7,7 @@ module MemoryBucketConcern
     scope :memory_4_to_8Gb,    -> { where("system_memory >= 4096 AND system_memory < 8192") }
     scope :memory_8_to_16Gb,   -> { where("system_memory >= 8192 AND system_memory < 16384") }
     scope :memory_16_to_32Gb,  -> { where("system_memory >= 16384 AND system_memory < 32768") }
-    scope :memory_32_64Gb,     -> { where("system_memory >= 32768 AND system_memory < 65536") }
+    scope :memory_32_to_64Gb,  -> { where("system_memory >= 32768 AND system_memory < 65536") }
     scope :memory_64_to_128Gb, -> { where("system_memory >= 65536 AND system_memory < 131072") }
     scope :memory_128Gb_plus,  -> { where("system_memory >= 131072") }
   end
