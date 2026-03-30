49cd97ff6d5c10e561589461a60932ab0b9c6e07
Stuart Auchterlonie <stuarta@squashedfrog.net>
2026-03-30 21:01:56 +0100

    move Myth::VersionBucket tests to the right place

--- test/models/myth/version_test.rb
+++ test/models/myth/version_test.rb
@@ -2,15 +2,15 @@ require "test_helper"
 
 class Myth::VersionTest < ActiveSupport::TestCase
   test "mythtv version bucket handling with plus sign" do
-    dbv = Myth::Version.find_or_create_by(version: "v31.0+fixes.202203040618.7e4ce1ba98~ubuntu20.04.1")
-    assert dbv.version_bucket == "v31.0"
+    dbv = Myth::Version.find_by(version: "v31.0+fixes.202203040618.7e4ce1ba98~ubuntu20.04.1")
+    assert dbv.valid?
   end
   test "mythtv version bucket handling with dashes" do
-    dbv = Myth::Version.find_or_create_by(version: "v36-Pre-492-gefe751de78")
-    assert dbv.version_bucket == "v36"
+    dbv = Myth::Version.find_by(version: "v36-Pre-492-gefe751de78")
+    assert dbv.valid?
   end
   test "mythtv version bucket handling with squiggle" do
-    dbv = Myth::Version.find_or_create_by(version: "v36.0~master.202508230548.20c2b86196~ubuntu25.04.1")
-    assert dbv.version_bucket == "v36.0"
+    dbv = Myth::Version.find_by(version: "v36.0~master.202508230548.20c2b86196~ubuntu25.04.1")
+    assert dbv.valid?
   end
 end
