fab5c95a0d93acb42d5921aa066559bf936ad00a
Stuart Auchterlonie <stuarta@squashedfrog.net>
2026-04-15 21:29:00 +0100

    fix omniauth init when configured with ENV variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,22 +6,21 @@ if keycloak.nil?
   keycloak[:host] = ENV["KEYCLOAK_HOST"] || "https://sso.mythtv.org"
   keycloak[:realm] = ENV["KEYCLOAK_REALM"] || "MythTV"
   keycloak[:redirect_uri] = ENV["KEYCLOAK_REDIRECT_URI"] || "https://smolt.mythtv.org/auth/openid_connect/callback"
-else
-  Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :openid_connect, {
-      name: :openid_connect,
-      issuer: "#{keycloak[:host]}/auth/realms/#{keycloak[:realm]}",
-      discovery: true,
-      scope: [ :openid, :email, :profile, :groups ],
-      response_type: :code,
-      uid_field: "preferred_username",
-      client_options: {
-        scheme: "https",
-        host: "sso.mythtv.org",
-        identifier: keycloak[:client_id],
-        secret: keycloak[:client_secret],
-        redirect_uri: keycloak[:redirect_uri]
-      }
+end
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :openid_connect, {
+    name: :openid_connect,
+    issuer: "#{keycloak[:host]}/auth/realms/#{keycloak[:realm]}",
+    discovery: true,
+    scope: [ :openid, :email, :profile, :groups ],
+    response_type: :code,
+    uid_field: "preferred_username",
+    client_options: {
+      scheme: "https",
+      host: "sso.mythtv.org",
+      identifier: keycloak[:client_id],
+      secret: keycloak[:client_secret],
+      redirect_uri: keycloak[:redirect_uri]
     }
-  end
+  }
 end
